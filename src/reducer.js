79616f46b1d2a9a8b00a88a0f8b4fa2aa1ecf01d
huangqinmeng <603400759@qq.com>
2018-03-02 11:04:46 +0800

    Delete reducer.js

--- src/reducer.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from 'redux'
-import {counter} from './index.redux'
-import {login1} from './login.redux'
-import {user}  from './redux/user.redux'
-import {chatuser}  from './redux/chatuser.redux'
-import {chat} from './redux/chat.redux'
-export default combineReducers({counter,login1,user,chatuser,chat})
